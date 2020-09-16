1e2b9ddcc2ab457420249f0d6e854f4f1ce09eef
Wingman <ryuash123@gmail.com>
2020-09-16 15:56:14 +0800

    fix ssr css problem

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,3 +1,5 @@
+import React from 'react';
+import Head from 'next/head';
 import { AppProps } from 'next/app';
 import CssBaseline from '@material-ui/core/CssBaseline';
 import { ApolloProvider } from '@apollo/client';
@@ -5,12 +7,34 @@ import client from '@graphql';
 import { appWithTranslation } from '../../i18n';
 
 // This default export is required in a new `pages/_app.js` file.
-function MyApp({ Component, pageProps }: AppProps) {
+function MyApp({
+  Component, pageProps,
+}: AppProps) {
+  React.useEffect(() => {
+    // Remove the server-side injected CSS.
+    const jssStyles = document.querySelector('#jss-server-side');
+    if (jssStyles) {
+      jssStyles.parentElement.removeChild(jssStyles);
+    }
+  }, []);
+
   return (
-    <ApolloProvider client={client}>
-      <CssBaseline />
-      <Component {...pageProps} />
-    </ApolloProvider>
+    <>
+      <Head>
+        <meta
+          name="viewport"
+          content="minimum-scale=1, initial-scale=1, width=device-width"
+        />
+      </Head>
+      <ApolloProvider
+        client={client}
+      >
+        <CssBaseline />
+        <Component
+          {...pageProps}
+        />
+      </ApolloProvider>
+    </>
   );
 }
 
