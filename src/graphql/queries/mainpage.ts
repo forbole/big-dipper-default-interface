dd6ffe12e510ce44b5c6c8ffb769b655b08a53f0
HarleyAppleChoi <appleno56@gmail.com>
2020-09-24 20:52:54 +0800

    mainpage scriptions

--- src/graphql/queries/mainpage.ts
+++ src/graphql/queries/mainpage.ts
@@ -13,37 +13,57 @@ export class block {
 }
 
 export const latestBlockHeight = gql`
-      query LatestBlockHeight {
+      subscription LatestBlockHeight {
   block(limit: 1, order_by: {height: desc}) {
     height
   }
 }`;
 
 export const AverageBlockTimeAllTime = gql`
-query AverageBlockTimeAllTime{
+subscription AverageBlockTimeAllTime{
   average_block_time_from_genesis(limit: 1, order_by: {height: desc}){
     average_time
   }
 }`;
 
 export const AverageBlockTimeLastMinute = gql`
-query AverageBlockTimeLastMinute{
+subscription AverageBlockTimeLastMinute{
   average_block_time_per_minute(limit: 1, order_by: {height: desc}){
     average_time
   }
 }`;
 
 export const AverageBlockTimeLastHour = gql`
-query AverageBlockTimeLastHour{
+subscription AverageBlockTimeLastHour{
   average_block_time_per_hour(limit: 1, order_by: {height: desc}){
     average_time
   }
 }`;
 
 export const AverageBlockTimeLastDay = gql`
-query AverageBlockTimeLastDay{
+subscription AverageBlockTimeLastDay{
   average_block_time_per_day(limit: 1, order_by: {height: desc}){
     average_time
   }
 }`;
 
+export const LatestBlocks = gql`
+subscription LatestBlocks {
+  block(limit: 9, order_by: {height: desc}) {
+    hash
+    proposer {
+      validator_infos {
+        validator_descriptions(order_by: {timestamp: desc}, limit: 1) {
+          moniker
+        }
+      }
+    }
+    height
+    num_txs
+    timestamp
+  }
+}`;
+
+export const LatestActivities = gql`
+
+`;
