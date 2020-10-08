30367e2c68b33a4c67d9537091f4ff96f455e591
YuhsinLiao1995 <61954949+YuhsinLiao1995@users.noreply.github.com>
2020-10-08 16:05:41 +0800

    update tests

--- tests/screens/about/index.test.tsx
+++ tests/screens/about/index.test.tsx
@@ -1,33 +1,17 @@
 import React from 'react';
 import { mount } from 'enzyme';
 import About from '@screens/about';
-import { WithMockApolloProvider } from '@tests/utils/mock_apollo_provider';
-import { queryExample } from '@graphql/queries';
 import { createMuiTheme } from '@material-ui/core/styles';
 import { lightTheme } from '@styles';
 import { WithMockMaterialTheme } from '@tests/utils/mock_material_theme';
 
-const mocks = [
-  {
-    request: {
-      query: queryExample,
-      variables: {
-      },
-    },
-  },
-];
-
 describe('About', () => {
   it('it renders', () => {
-    const wrapper = mount(
-      WithMockApolloProvider({
-        component: WithMockMaterialTheme({
-          component: <About />,
-          theme: createMuiTheme(lightTheme),
-        }),
-        mocks,
-      }),
-    );
+    const wrapper = mount(WithMockMaterialTheme({
+      component: <About />,
+      theme: createMuiTheme(lightTheme),
+    }));
+
     expect(
       wrapper.find('span').first().text(),
     ).toBe('aboutbigDipper');
