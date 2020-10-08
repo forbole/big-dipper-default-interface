e633e4846d0500ad64117eeef67902b85df1c748
Wingman <ryuash123@gmail.com>
2020-10-08 10:02:11 +0800

    starting point

---
+++ src/pages/about.tsx
@@ -0,0 +1,9 @@
+import About from '@screens/about';
+
+const AboutPage = () => {
+  return (
+    <About />
+  );
+};
+
+export default AboutPage;
---
+++ src/screens/about/index.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import { Layout } from '@components';
+
+// Starting point for you
+// work on mobile structure
+// work on mobile css
+// work your way up to tablet -> desktop
+// add testing
+const About = () => {
+  return (
+    <Layout>
+      about stuff here
+    </Layout>
+  );
+};
+
+export default About;
