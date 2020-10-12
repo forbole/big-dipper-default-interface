6e9601dcf14cf6fe130a75efadb8b09467458e4c
Wingman <ryuash123@gmail.com>
2020-10-12 21:33:50 +0800

    small css change

--- src/screens/about/index.tsx
+++ src/screens/about/index.tsx
@@ -12,15 +12,13 @@ const About = () => {
   return (
     <Layout>
       <MaxWidthContainer>
-        <MaxWidthContent>
-          <div className={classes.root}>
-            <h1 className={classes.title}>
-              {t('aboutbigDipper')}
-            </h1>
-            <p className={classes.text}>
-              {t('paragraph')}
-            </p>
-          </div>
+        <MaxWidthContent className={classes.root}>
+          <h1 className={classes.title}>
+            {t('aboutbigDipper')}
+          </h1>
+          <p className={classes.text}>
+            {t('paragraph')}
+          </p>
         </MaxWidthContent>
       </MaxWidthContainer>
     </Layout>
