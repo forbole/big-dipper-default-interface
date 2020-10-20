1052715daa3b62fbcb523bb4c9f7f6be58f76420
HarleyAppleChoi <appleno56@gmail.com>
2020-10-20 17:19:28 +0800

    wingman

--- tests/screens/home/index.test.tsx
+++ tests/screens/home/index.test.tsx
@@ -3,7 +3,6 @@ import { mount } from 'enzyme';
 import Home from '@screens/home';
 import { WithMockApolloProvider } from '@tests/utils/mock_apollo_provider';
 import { awaitActions } from '@tests/utils/await_actions';
-import { queryExample } from '@graphql/queries';
 import { createMuiTheme } from '@material-ui/core/styles';
 import { lightTheme } from '@styles';
 import { WithMockMaterialTheme } from '@tests/utils/mock_material_theme';
@@ -11,7 +10,6 @@ import { WithMockMaterialTheme } from '@tests/utils/mock_material_theme';
 const mocks = [
   {
     request: {
-      query: queryExample,
       variables: {
       },
     },
