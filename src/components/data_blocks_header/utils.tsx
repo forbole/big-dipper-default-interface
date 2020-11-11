3b871606565d7f9ad90def4b5055f44d542fbd41
Wingman <ryuash123@gmail.com>
2020-11-11 15:31:59 +0800

    wip shit

--- src/components/data_blocks_header/utils.tsx
+++ src/components/data_blocks_header/utils.tsx
@@ -1,23 +1,3 @@
-// WithCustom.args = {
-//   label: 'Latest Block Height',
-//   value: '15,000',
-//   selectedValue: '24',
-//   customComponent: <DataBlockCustom />,
-//   durations: [{
-//     value: '7_hours',
-//     display: 'Last 7 hours',
-//     callback: (key) => {
-//       console.log(`called with key: ${key}`);
-//     },
-//   }, {
-//     value: '24',
-//     display: '24 hours',
-//     callback: (key) => {
-//       console.log(`called with key: ${key}`);
-//     },
-//   }],
-// };
-
 export const averageBlockTimeDurationKeys = [
   'allTime',
   'lastMinute',
