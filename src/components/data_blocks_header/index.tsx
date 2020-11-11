86035bb7531963b85faa184cf7ad429defca94fa
Wingman <ryuash123@gmail.com>
2020-11-11 15:22:33 +0800

    wip

--- src/components/data_blocks_header/index.tsx
+++ src/components/data_blocks_header/index.tsx
@@ -9,31 +9,40 @@ import { useDataBlocksHeaderHook } from './hooks';
 const DataBlocksHeader = () => {
   const { classes } = useGetStyles();
   const { t } = useTranslation('common');
+  console.log(t('latestBlockHeight'), 'wtf');
   const { handleBlockTimeDurationClick } = useDataBlocksHeaderHook();
   const blockTimeDurations = getBlocktimeDurations(t);
   return (
     <div className={classnames(classes.root, 'data-blocks-container')}>
       <DataBlock
         label={t('latestBlockHeight')}
-        value="15,000"
+        value="2,768,643"
         className="latest-block-height"
       />
       <DataBlock
         label={t('averageBlockTime')}
-        value="15,000"
+        value="7.11 s"
         durations={blockTimeDurations}
         durationsCallback={handleBlockTimeDurationClick}
         className="average-block-time"
       />
       <DataBlock
         label={t('price')}
-        value="15,000"
+        value="$ 2.75"
         className="price"
+        durations={[{
+          value: '24_hour',
+          display: '24h',
+        }]}
       />
       <DataBlock
         label={t('activeValidators')}
-        value="15,000"
+        value="125"
         className="active-validators"
+        durations={[{
+          value: '',
+          display: 'out of 246 validators',
+        }]}
       />
     </div>
   );
