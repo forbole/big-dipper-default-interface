400037f01d9f0169ba13fc491f21e0607958b06f
Wingman <ryuash123@gmail.com>
2020-11-12 11:32:42 +0800

    add: activities layout

---
+++ src/screens/activities/index.tsx
@@ -0,0 +1,66 @@
+import React from 'react';
+import classnames from 'classnames';
+import { useTranslation } from 'i18n';
+import { DesktopHeaderBar } from 'big-dipper-internal-ui';
+import { Layout } from '@components';
+import { useActivitiesHook } from './hooks';
+import { useGetStyles } from './styles';
+
+const Validators = () => {
+  const { t } = useTranslation(['activities', 'common']);
+  const { handleSearchbarSubmit } = useActivitiesHook();
+  const { classes } = useGetStyles();
+  return (
+    <Layout
+      header={(
+        <DesktopHeaderBar
+          title={t('title')}
+          market={[
+            {
+              key: 'Price',
+              value: '$2.75',
+            },
+            {
+              key: 'Market Cap',
+              value: '$515,555.75',
+            },
+            {
+              key: 'Inflation',
+              value: '7.95%',
+            },
+            {
+              key: 'Community Pool',
+              value: '3,400,000 ATOM',
+            },
+          ]}
+        />
+)}
+      searchBar={{
+        searchBarPlaceholder: t('common:searchbar'),
+        searchBarCallback: handleSearchbarSubmit,
+      }}
+    >
+      {/* ===================================== */}
+      {/* content start */}
+      {/* ===================================== */}
+      <div className={classnames(classes.root)}>
+        <div className={classnames('data-blocks')}>
+          data blocks
+        </div>
+        <div className={classnames('content-container')}>
+          <div className={classnames('mobile-tablet-header')}>
+            mobile tablet header
+          </div>
+          <div className={classnames('activities-content')}>
+            Activities table
+          </div>
+        </div>
+      </div>
+      {/* ===================================== */}
+      {/* content end */}
+      {/* ===================================== */}
+    </Layout>
+  );
+};
+
+export default Validators;
---
+++ src/screens/activities/styles.tsx
@@ -0,0 +1,42 @@
+import {
+  makeStyles, createStyles,
+} from '@material-ui/styles';
+import { getMinMediaQuery } from '@styles';
+
+export const useGetStyles = () => {
+  const useStyles = makeStyles((theme: any) => createStyles({
+    root: {
+      display: 'grid',
+      gridGap: theme?.palette?.custom?.gutters?.small,
+      padding: theme?.palette?.custom?.gutters?.small,
+      '& > *': {
+        background: 'pink',
+      },
+      '& .data-blocks': {
+        display: 'none',
+      },
+      '& .mobile-tablet-header': {
+        height: '100px',
+      },
+      '& .activities-content': {
+        height: '500px',
+      },
+      [getMinMediaQuery(theme?.breakpoints?.values?.tablet)]: {
+
+      },
+      [getMinMediaQuery(theme?.breakpoints?.values?.desktop)]: {
+        '& .data-blocks': {
+          display: 'block',
+          height: '150px',
+        },
+        '& .mobile-tablet-header': {
+          display: 'none',
+        },
+      },
+    },
+  }));
+
+  return {
+    classes: useStyles(),
+  };
+};
