d5a2bd080568f58f2b1b755ecc6b78fa952a0fcb
Wingman <ryuash123@gmail.com>
2020-11-12 11:37:16 +0800

    add: proposals layout

---
+++ src/pages/proposals/index.tsx
@@ -0,0 +1,9 @@
+import Proposals from '@screens/proposals';
+
+const ProposalsPage = () => {
+  return (
+    <Proposals />
+  );
+};
+
+export default ProposalsPage;
---
+++ src/screens/proposals/index.tsx
@@ -0,0 +1,66 @@
+import React from 'react';
+import classnames from 'classnames';
+import { useTranslation } from 'i18n';
+import { DesktopHeaderBar } from 'big-dipper-internal-ui';
+import { Layout } from '@components';
+import { useProposalHook } from './hooks';
+import { useGetStyles } from './styles';
+
+const Validators = () => {
+  const { t } = useTranslation(['proposals', 'common']);
+  const { handleSearchbarSubmit } = useProposalHook();
+  const { classes } = useGetStyles();
+  return (
+    <Layout
+      header={(
+        <DesktopHeaderBar
+          title={t('title')}
+          market={[
+            {
+              key: 'Price',
+              value: '$2.75',
+            },
+            {
+              key: 'Market Cap',
+              value: '$515,555.75',
+            },
+            {
+              key: 'Inflation',
+              value: '7.95%',
+            },
+            {
+              key: 'Community Pool',
+              value: '3,400,000 ATOM',
+            },
+          ]}
+        />
+)}
+      searchBar={{
+        searchBarPlaceholder: t('common:searchbar'),
+        searchBarCallback: handleSearchbarSubmit,
+      }}
+    >
+      {/* ===================================== */}
+      {/* content start */}
+      {/* ===================================== */}
+      <div className={classnames(classes.root)}>
+        <div className={classnames('data-blocks')}>
+          data blocks
+        </div>
+        <div className={classnames('content-container')}>
+          <div className={classnames('mobile-tablet-header')}>
+            mobile tablet header
+          </div>
+          <div className={classnames('proposals-content')}>
+            Proposals table
+          </div>
+        </div>
+      </div>
+      {/* ===================================== */}
+      {/* content end */}
+      {/* ===================================== */}
+    </Layout>
+  );
+};
+
+export default Validators;
