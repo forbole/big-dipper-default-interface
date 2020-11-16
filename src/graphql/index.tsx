f0ba130461a3ef6439edb5fb45d7f6d200198cce
Wingman <ryuash123@gmail.com>
2020-11-16 17:08:50 +0800

    wip: setting up websocket

--- src/graphql/index.tsx
+++ src/graphql/index.tsx
@@ -1,9 +1,35 @@
 import {
-  ApolloClient, InMemoryCache,
+  ApolloClient,
+  InMemoryCache,
+  split, HttpLink,
 } from '@apollo/client';
+import { getMainDefinition } from '@apollo/client/utilities';
+import { WebSocketLink } from '@apollo/client/link/ws';
 
-export const client = new ApolloClient({
+const httpLink = new HttpLink({
   uri: process.env.NEXT_PUBLIC_GRAPHQL_URL,
+});
+
+const wsLink = new WebSocketLink({
+  uri: process.env.NEXT_PUBLIC_GRAPHQL_WS,
+  options: {
+    reconnect: true,
+  },
+});
+
+const link = split(
+  ({ query }) => {
+    const {
+      kind, operation,
+    }:any = getMainDefinition(query);
+    return kind === 'OperationDefinition' && operation === 'subscription';
+  },
+  wsLink,
+  httpLink,
+);
+
+const client = new ApolloClient({
+  link,
   cache: new InMemoryCache(),
 });
 
