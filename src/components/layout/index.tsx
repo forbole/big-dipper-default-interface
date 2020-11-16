f93fb0e797f90c2e99c95bf5ea26edf89b088803
Wingman <ryuash123@gmail.com>
2020-11-16 12:04:25 +0800

    update: header

--- src/components/layout/index.tsx
+++ src/components/layout/index.tsx
@@ -1,4 +1,7 @@
 import React, { useContext } from 'react';
+import { useRouter } from 'next/router';
+import validator from 'validator';
+import Head from 'next/head';
 import classnames from 'classnames';
 import { useTranslation } from 'i18n';
 import {
@@ -21,6 +24,7 @@ import { NetworkItem } from './components';
 import { useGetStyles } from './styles';
 
 export const Layout = (props: LayoutProps) => {
+  const baseUrl = process.env.NEXT_PUBLIC_URL;
   const { t } = useTranslation('common');
   const { classes } = useGetStyles();
   const {
@@ -28,7 +32,12 @@ export const Layout = (props: LayoutProps) => {
     className = '',
     searchBar,
     header,
+    description = '',
+    type = 'website',
+    title = t('bigDipper'),
   } = props;
+
+  let { image = '/images/icons/favicon-32x32.png' } = props;
 
   // ============================
   // Global props
@@ -43,6 +52,7 @@ export const Layout = (props: LayoutProps) => {
   const windowSize = useGetScreenSize();
   const mobileHook = useMobileNavHook(windowSize);
   const desktopHook = useDesktopNavHook(windowSize);
+  const router = useRouter();
   // ============================
   // Languages
   // ============================
@@ -76,78 +86,108 @@ export const Layout = (props: LayoutProps) => {
   // ============================
   // Menu
   // ============================
+  const currentPath = `${process.env.NEXT_PUBLIC_URL}${router.asPath}`;
   const menuItems = getNavComponents(t);
+  // ============================
+  // Meta Tags
+  // ============================
+  if (!validator.isURL(image)) {
+    image = `${baseUrl}${image}`;
+  }
   return (
-    <div className={classnames(classes.root, className)}>
-      {/* ========================================= */}
-      {/* desktop nav start */}
-      {/* ========================================= */}
-      <DesktopNav
-        sideBar={{
-          open: desktopHook.isMenuOpen,
-          onClick: desktopHook.toggleMenu,
-          items: menuItems,
-        }}
-        topBar={{
-          network: {
-            selectedNetwork,
-            isNetworkOpen: desktopHook.isNetworkOpen,
+    <>
+      <Head>
+        <title>{title}</title>
+        <meta name="description" content={description} />
+        <meta name="og:type" content={type} />
+        <meta name="og:title" content={title} />
+        <meta name="og:site_name" content="Big Dipper" />
+        <meta name="og:url" content={currentPath} />
+        <meta name="og:description" content={description} />
+        <meta name="og:image" content={image} />
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta name="twitter:image" content={image} />
+        <link rel="apple-touch-icon" sizes="180x180" href={`${baseUrl}/images/icons/apple-touch-icon.png`} />
+        <link rel="icon" type="image/png" sizes="32x32" href={`${baseUrl}/images/icons/favicon-32x32.png`} />
+        <link rel="icon" type="image/png" sizes="16x16" href={`${baseUrl}/images/icons/favicon-16x16.png`} />
+        <link rel="manifest" href={`${baseUrl}/images/icons/site.webmanifest`} />
+        <link rel="mask-icon" href={`${baseUrl}/images/icons/safari-pinned-tab.svg`} color="#5bbad5" />
+        <link rel="shortcut icon" href={`${baseUrl}/images/icons/favicon.ico`} />
+        <meta name="msapplication-TileColor" content="#da532c" />
+        <meta name="msapplication-config" content={`${baseUrl}/images/icons/browserconfig.xml`} />
+        <meta name="theme-color" content="#ffffff" />
+      </Head>
+      <div className={classnames(classes.root, className)}>
+        {/* ========================================= */}
+        {/* desktop nav start */}
+        {/* ========================================= */}
+        <DesktopNav
+          sideBar={{
+            open: desktopHook.isMenuOpen,
+            onClick: desktopHook.toggleMenu,
+            items: menuItems,
+          }}
+          topBar={{
+            network: {
+              selectedNetwork,
+              isNetworkOpen: desktopHook.isNetworkOpen,
+              items: networkItems,
+              toggleNetwork: desktopHook.toggleNetwork,
+            },
+            header,
+            language,
+            themeMode,
+            searchBar,
+          }}
+        />
+        {/* ========================================= */}
+        {/* desktop nav end */}
+        {/* ========================================= */}
+        {/* ========================================= */}
+        {/* mobile nav start */}
+        {/* ========================================= */}
+        <MobileNav
+          hamburgerIcon={{
+            isOpen: mobileHook.isOpen,
+            onClick: mobileHook.toggleNavMenus,
+          }}
+          logo={{
+            alt: 'big dipper logo',
+            onClick: mobileHook.returnToHome,
+          }}
+          menu={{
+            themeMode,
+            language,
+            isMenuOpen: mobileHook.isMenuOpen,
+            items: menuItems,
+          }}
+          network={{
+            isNetworkOpen: mobileHook.isNetworkOpen,
             items: networkItems,
-            toggleNetwork: desktopHook.toggleNetwork,
-          },
-          header,
-          language,
-          themeMode,
-          searchBar,
-        }}
-      />
-      {/* ========================================= */}
-      {/* desktop nav end */}
-      {/* ========================================= */}
-      {/* ========================================= */}
-      {/* mobile nav start */}
-      {/* ========================================= */}
-      <MobileNav
-        hamburgerIcon={{
-          isOpen: mobileHook.isOpen,
-          onClick: mobileHook.toggleNavMenus,
-        }}
-        logo={{
-          alt: 'big dipper logo',
-          onClick: mobileHook.returnToHome,
-        }}
-        menu={{
-          themeMode,
-          language,
-          isMenuOpen: mobileHook.isMenuOpen,
-          items: menuItems,
-        }}
-        network={{
-          isNetworkOpen: mobileHook.isNetworkOpen,
-          items: networkItems,
-          toggleNetwork: mobileHook.openNetwork,
-          selectedNetwork,
-        }}
-        searchBar={searchBar}
-      />
-      {/* ========================================= */}
-      {/* mobile nav end */}
-      {/* ========================================= */}
-      {/* ========================================= */}
-      {/* content start */}
-      {/* ========================================= */}
-      <div
-        className={classnames('children-wrapper', {
-          desktopOpen: desktopHook.isMenuOpen,
-        })}
-      >
-        {children}
+            toggleNetwork: mobileHook.openNetwork,
+            selectedNetwork,
+          }}
+          searchBar={searchBar}
+        />
+        {/* ========================================= */}
+        {/* mobile nav end */}
+        {/* ========================================= */}
+        {/* ========================================= */}
+        {/* content start */}
+        {/* ========================================= */}
+        <div
+          className={classnames('children-wrapper', {
+            desktopOpen: desktopHook.isMenuOpen,
+          })}
+        >
+          {children}
+        </div>
+        {/* ========================================= */}
+        {/* content end */}
+        {/* ========================================= */}
+        <Footer />
       </div>
-      {/* ========================================= */}
-      {/* content end */}
-      {/* ========================================= */}
-      <Footer />
-    </div>
+    </>
   );
 };
 
