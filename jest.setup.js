16a1a83948ec97961e52fba15bd9401aaa0fca2d
Wingman <ryuash123@gmail.com>
2020-11-16 12:17:57 +0800

    update: jest settings

--- jest.setup.js
+++ jest.setup.js
@@ -4,6 +4,7 @@ const Adapter = require('enzyme-adapter-react-16');
 const { setConfig } = require('next/config');
 const { initReactI18next } = require('./tests/utils/mock_react_i18next');
 const { publicRuntimeConfig } = require('./next.config');
+// const nextRouter = require('next/router');
 
 publicRuntimeConfig.use = [initReactI18next];
 
@@ -17,20 +18,13 @@ configure({
 // ============================
 // Mock next/router
 // ============================
-jest.mock('next/router', () => ({
-  // spread out all "Router" exports
-  ...jest.requireActual('next/router'),
-
-  // shallow merge the "default" exports with...
-  default: {
-    // all actual "default" exports...
-    ...jest.requireActual('next/router').default,
-
-    // and overwrite push and replace to be jest functions
-    push: jest.fn(),
-    replace: jest.fn(),
+jest.mock("next/router", () => ({
+  useRouter() {
+      return {
+          route: "/",
+          pathname: "",
+          query: "",
+          asPath: "",
+      };
   },
 }));
-
-// export the mocked instance above
-module.exports = jest.requireMock('next/router');
