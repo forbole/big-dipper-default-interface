e9c05c1e016378e500cb44a283e1c8b332f74db5
Wingman <ryuash123@gmail.com>
2020-11-17 12:59:21 +0800

    blegdfhh

--- src/components/header_bar/index.tsx
+++ src/components/header_bar/index.tsx
@@ -1,9 +1,21 @@
 import React from 'react';
 import { DesktopHeaderBar } from 'big-dipper-internal-ui';
+import { chainConfig } from '@src/chain_config';
 import { HeaderBarProps } from './types';
+import { useHeaderBarHook } from './hooks';
+import {
+  formatDenom, getBasePool,
+} from './utils';
 
+/**
+ * Custom Header component
+ */
 const HeaderBar = (props:HeaderBarProps) => {
   const { title } = props;
+  const { communityPool } = useHeaderBarHook();
+  const baseCommunityPool = getBasePool(chainConfig.base, communityPool.data);
+  const communityPoolValue = formatDenom('daric', baseCommunityPool.amount);
+  console.log(communityPoolValue, 'whats the value');
   return (
     <DesktopHeaderBar
       title={title}
