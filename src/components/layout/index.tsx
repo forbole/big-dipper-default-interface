0b40e2d9d37709b501c4b7683254a0c465a4a239
Wingman <ryuash123@gmail.com>
2020-11-18 13:34:22 +0800

    update: seo

--- src/components/layout/index.tsx
+++ src/components/layout/index.tsx
@@ -98,61 +98,57 @@ export const Layout = (props: LayoutProps) => {
   }
   return (
     <>
+      {/* ========================================= */}
+      {/* meta tags */}
+      {/* ========================================= */}
+      <NextSeo
+        title={title}
+        description={description}
+        openGraph={{
+          type,
+          title,
+          site_name: 'Big Dipper',
+          url: currentPath,
+          description,
+          images: [
+            {
+              url: image,
+              alt: imageAlt ?? description,
+            },
+          ],
+        }}
+        twitter={{
+          cardType: 'summary_large_image',
+        }}
+        additionalMetaTags={[
+          {
+            name: 'msapplication-TileColor',
+            content: '#da532c',
+          },
+          {
+            name: 'msapplication-config',
+            content: `${baseUrl}/images/icons/browserconfig.xml`,
+          },
+          {
+            name: 'theme-color',
+            content: '#ffffff',
+          },
+        ]}
+      />
+      {/* ========================================= */}
+      {/* header links */}
+      {/* ========================================= */}
       <Head>
-        <NextSeo
-          title={title}
-          description={description}
-          openGraph={{
-            type,
-            title,
-            site_name: 'Big Dipper',
-            url: currentPath,
-            description,
-            images: [
-              {
-                url: image,
-                alt: imageAlt ?? description,
-              },
-            ],
-          }}
-          twitter={{
-            cardType: 'summary_large_image',
-          }}
-          additionalMetaTags={[
-            {
-              name: 'msapplication-TileColor',
-              content: '#da532c',
-            },
-            {
-              name: 'msapplication-config',
-              content: `${baseUrl}/images/icons/browserconfig.xml`,
-            },
-            {
-              name: 'theme-color',
-              content: '#ffffff',
-            },
-          ]}
-        />
-        {/* <title>{title}</title> */}
-        {/* <meta name="description" content={description} /> */}
-        {/* <meta property="og:type" content={type} /> */}
-        {/* <meta property="og:title" content={title} /> */}
-        {/* <meta property="og:site_name" content="Big Dipper" /> */}
-        {/* <meta property="og:url" content={currentPath} /> */}
-        {/* <meta property="og:description" content={description} /> */}
-        {/* <meta property="og:image" content={image} /> */}
-        {/* <meta name="twitter:card" content="summary_large_image" /> */}
-        {/* <meta name="twitter:image" content={image} /> */}
         <link rel="apple-touch-icon" sizes="180x180" href={`${baseUrl}/images/icons/apple-touch-icon.png`} />
         <link rel="icon" type="image/png" sizes="32x32" href={`${baseUrl}/images/icons/favicon-32x32.png`} />
         <link rel="icon" type="image/png" sizes="16x16" href={`${baseUrl}/images/icons/favicon-16x16.png`} />
         <link rel="manifest" href={`${baseUrl}/images/icons/site.webmanifest`} />
         <link rel="mask-icon" href={`${baseUrl}/images/icons/safari-pinned-tab.svg`} color="#5bbad5" />
         <link rel="shortcut icon" href={`${baseUrl}/images/icons/favicon.ico`} />
-        {/* <meta name="msapplication-TileColor" content="#da532c" /> */}
-        {/* <meta name="msapplication-config" content={`${baseUrl}/images/icons/browserconfig.xml`} /> */}
-        {/* <meta name="theme-color" content="#ffffff" /> */}
       </Head>
+      {/* ========================================= */}
+      {/* body */}
+      {/* ========================================= */}
       <div className={classnames(classes.root, className)}>
         {/* ========================================= */}
         {/* desktop nav start */}
