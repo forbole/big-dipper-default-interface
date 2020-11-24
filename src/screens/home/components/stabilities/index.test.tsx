a781c878b94baf8b0d4f0f5ba1525a20ec7025c9
YuhsinLiao1995 <61954949+YuhsinLiao1995@users.noreply.github.com>
2020-11-24 18:22:57 +0800

    update unit test

--- src/screens/home/components/stabilities/index.test.tsx
+++ src/screens/home/components/stabilities/index.test.tsx
@@ -2,17 +2,54 @@ import React from 'react';
 import { mount } from 'enzyme';
 import { BaseWrapper } from '@tests/utils/base_wrapper';
 import { lightTheme } from '@styles';
-import Stabilities from './index';
+import { Stabilities } from 'big-dipper-default-ui';
 
 describe('Stabilities', () => {
   it('correctly renders component', () => {
     const wrapper = mount(
       BaseWrapper({
-        component: <Stabilities />,
+        component: <Stabilities
+          title="Stabilities"
+          coin="atom"
+          price={{
+            title: 'Price',
+            display: '$515,551,345.666',
+          }}
+          inflation={{
+            title: 'inflation',
+            display: '7 %',
+          }}
+          marketCap={{
+            title: 'Market Cap',
+            display: '$15,000',
+          }}
+          communityPool={{
+            title: 'Community Pool',
+            display: '370,000',
+          }}
+          data={{
+            total: {
+              value: 300000,
+              display: '0.3 m',
+            },
+            detail: [
+              {
+                title: 'Bonded',
+                value: 100000,
+                display: '100,000',
+              },
+              {
+                title: 'UnBonded',
+                value: 200000,
+                display: '200,000',
+              },
+            ],
+          }}
+          colors={['#FF7846', '#FFD800']}
+        />,
         theme: lightTheme,
       }),
     );
     expect(wrapper).not.toBeNull();
-    expect(wrapper.find('h1').first().text()).toEqual('stabilitiesTitle');
   });
 });
