41d74a92a99aadb875aedbe3e1490753ddc9fc8f
Wingman <ryuash123@gmail.com>
2020-11-26 17:12:07 +0800

    add: block details page

--- src/screens/block_details/index.tsx
+++ src/screens/block_details/index.tsx
@@ -1,8 +1,50 @@
 import React from 'react';
+import classnames from 'classnames';
+import { useTranslation } from 'i18n';
+import {
+  Layout,
+  HeaderBar,
+  DataBlocksHeader,
+} from '@components';
+import { useValidatorDetailsHook } from './hooks';
+import { useGetStyles } from './styles';
 
 const BlockDetails = () => {
+  const { t } = useTranslation(['blocks', 'common']);
+  const { handleSearchbarSubmit } = useValidatorDetailsHook();
+  const { classes } = useGetStyles();
   return (
-    <div>block details</div>
+    <Layout
+      header={(
+        <HeaderBar title={t('subTitle')} />
+      )}
+      searchBar={{
+        searchBarPlaceholder: t('common:searchbar'),
+        searchBarCallback: handleSearchbarSubmit,
+      }}
+    >
+      {/* ===================================== */}
+      {/* content start */}
+      {/* ===================================== */}
+      <div className={classnames(classes.root)}>
+        <div className={classnames('data-blocks')}>
+          <DataBlocksHeader />
+        </div>
+        <div className={classnames('mobile-tablet-header')}>
+          mobile tablet header
+        </div>
+        <div className={classnames('block-details')}>
+          block details
+        </div>
+        <div className={classnames('block-activities')}>
+          block activities
+        </div>
+      </div>
+      {/* ===================================== */}
+      {/* content end */}
+      {/* ===================================== */}
+    </Layout>
+
   );
 };
 
