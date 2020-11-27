4b04cd3a9c1f4706bc702f1df679bfa40b4322db
Wingman <ryuash123@gmail.com>
2020-11-27 10:19:15 +0800

    add: activity details page

--- src/screens/activities_details/index.tsx
+++ src/screens/activities_details/index.tsx
@@ -1,10 +1,48 @@
 import React from 'react';
+import classnames from 'classnames';
+import { useTranslation } from 'i18n';
+import {
+  Layout,
+  DataBlocksHeader,
+  HeaderBar,
+} from '@components';
+import { useActivitiesDetailsHook } from './hooks';
+import { useGetStyles } from './styles';
 
 const ActivitiesDetails = () => {
+  const { t } = useTranslation(['activities', 'common']);
+  const { handleSearchbarSubmit } = useActivitiesDetailsHook();
+  const { classes } = useGetStyles();
   return (
-    <div>
-      activitiy details
-    </div>
+    <Layout
+      header={(
+        <HeaderBar title={t('subTitle')} />
+      )}
+      searchBar={{
+        searchBarPlaceholder: t('common:searchbar'),
+        searchBarCallback: handleSearchbarSubmit,
+      }}
+    >
+      {/* ===================================== */}
+      {/* content start */}
+      {/* ===================================== */}
+      <div className={classnames(classes.root)}>
+        <div className={classnames('data-blocks')}>
+          <DataBlocksHeader />
+        </div>
+        <div className={classnames('content-container')}>
+          <div className={classnames('mobile-tablet-header')}>
+            mobile tablet header
+          </div>
+          <div className={classnames('activity-details-content')}>
+            Activities Details table
+          </div>
+        </div>
+      </div>
+      {/* ===================================== */}
+      {/* content end */}
+      {/* ===================================== */}
+    </Layout>
   );
 };
 
