7061c2a4ab4b92569d7f4ab3a82ce09d4d291d78
Wingman <ryuash123@gmail.com>
2020-12-01 15:32:28 +0800

    wip: need to review

--- src/screens/home/components/online_voting_power/styles.tsx
+++ src/screens/home/components/online_voting_power/styles.tsx
@@ -1,6 +1,7 @@
 import {
   makeStyles, createStyles,
 } from '@material-ui/styles';
+import { getMinMediaQuery } from '@styles';
 
 export const useGetStyles = () => {
   const useStyles = makeStyles((theme: any) => createStyles({
@@ -9,6 +10,11 @@ export const useGetStyles = () => {
       // color: theme?.palette?.custom?.fonts?.fontThree,
       // fontSize: '1.125rem',
       // margin: 0,
+      [getMinMediaQuery(theme?.breakpoints?.values?.desktop)]: {
+        '& .selected-title': {
+          fontSize: '1.5rem',
+        },
+      },
     },
   }));
 
