cc8cd342ad2392ea2da3b2b0afa115fa9b9a5e5b
Wingman <ryuash123@gmail.com>
2020-12-09 11:41:50 +0800

    add: testing

---
+++ src/screens/validator_details/components/info/index.test.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import { ValidatorInfo } from 'big-dipper-default-ui';
+import { mount } from 'enzyme';
+import { BaseWrapper } from '@tests/utils/base_wrapper';
+import { lightTheme } from '@styles';
+import Info from '.';
+
+describe('Info', () => {
+  it('correctly renders component', () => {
+    const wrapper = mount(
+      BaseWrapper({
+        component: <Info />,
+        theme: lightTheme,
+      }),
+    );
+    expect(wrapper).not.toBeNull();
+    expect(wrapper.find(ValidatorInfo)).toHaveLength(1);
+  });
+});
