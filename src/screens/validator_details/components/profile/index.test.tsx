cbb6f3749841e5e3d85bf32b6629730855aadf55
ryuash <ryuash123@gmail.com>
2021-01-05 16:13:26 +0800

    ran lint and test

---
+++ src/screens/validator_details/components/profile/index.test.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import { ValidatorProfile } from 'big-dipper-default-ui';
+import { mount } from 'enzyme';
+import { BaseWrapper } from '@tests/utils/base_wrapper';
+import { lightTheme } from '@styles';
+import Profile from '.';
+
+describe('Profile', () => {
+  it('correctly renders component', () => {
+    const wrapper = mount(
+      BaseWrapper({
+        component: <Profile />,
+        theme: lightTheme,
+      }),
+    );
+    expect(wrapper).not.toBeNull();
+    expect(wrapper.find(ValidatorProfile)).toHaveLength(1);
+  });
+});
