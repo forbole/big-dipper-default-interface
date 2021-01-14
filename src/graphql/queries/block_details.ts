d1fd137b010aa0a770caa4f71e94b15f7cfad34d
ryuash <ryuash123@gmail.com>
2021-01-14 14:01:41 +0800

    .

---
+++ src/graphql/queries/block_details.ts
@@ -0,0 +1,42 @@
+export const BLOCK_DETAILS = `
+  query BlockDetail ($height: bigint){
+    block(limit: 1, where: {height: {_eq: $height}}) {
+      hash
+      timestamp
+      num_txs
+      validator {
+        validator_description {
+          moniker
+          identity
+          validator_address
+        }
+      }
+      pre_commits
+    }
+  }
+`;
+
+// export const BLOCK_DETAIL = `
+//   query BlockDetail ($height: bigint){
+//     block(limit: 1, where: {height: {_eq: $height}}) {
+//       hash
+//       timestamp
+//       num_txs
+//       validator {
+//         validator_description {
+//           moniker
+//           identity
+// validator_address
+//         }
+//       }
+//       pre_commits
+//       validator_voting_power_histories_aggregate {
+//         aggregate {
+//           sum {
+//             voting_power
+//           }
+//         }
+//       }
+//     }
+//   }
+// `;
--- src/models/latest_block.ts
+++ src/models/latest_block.ts
@@ -5,7 +5,6 @@ class LatestBlock {
   public tx: number;
   public hash: string;
   public timestamp: string;
-  public success: boolean;
   public validator: {
     moniker: string;
     validatorAddress: string;
@@ -18,7 +17,6 @@ class LatestBlock {
     this.hash = payload.hash;
     this.timestamp = payload.timestamp;
     this.validator = payload.validator;
-    this.success = payload.success;
   }
 
   static fromJson(json: any) {
@@ -26,7 +24,6 @@ class LatestBlock {
       height: json.height,
       tx: json.num_txs,
       hash: json.hash,
-      success: json.success,
       timestamp: json.timestamp,
       validator: {
         moniker: R.pathOr('', ['validator', 'validator_description', 'moniker'], json),
