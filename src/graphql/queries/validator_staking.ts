8dd2472dfc22a481758ab715cfd45e068b677f88
Wingman L <42913823+ryuash@users.noreply.github.com>
2021-02-09 13:01:48 +0800

    Update/validator staking query (#195)

--- src/graphql/queries/validator_staking.ts
+++ src/graphql/queries/validator_staking.ts
@@ -1,19 +1,20 @@
 export const VALIDATOR_STAKING = `
-query ValidatorStaking($address: String, $height: bigint) {
+query ValidatorStaking($address: String, $height: bigint, $time: timestamp) {
   validator(where: {_or: [{validator_info: {consensus_address: {_eq: $address}}}, {validator_info: {operator_address: {_eq: $address}}}]}) {
     delegations(where: {height: {_eq: $height}}) {
       amount
       delegator_address
     }
-    redelegations (where: {height: {_eq: $height}}) {
+    redelegations(where: { completion_time: {_gt: $time}}, order_by: {completion_time: desc}) {
       src_validator_address
       dst_validator_address
       delegator_address
       amount
     }
-    unbonding_delegations (where: {height: {_eq: $height}}) {
+    unbonding_delegations(where: { completion_timestamp: {_gt: $time}}, order_by: {completion_timestamp: desc}) {
       amount
       delegator_address
+      completion_timestamp
     }
   }
 }
