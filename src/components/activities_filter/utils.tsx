17a9b806566cc14d7f1e9b8a0a25e64f19ad8560
Wingman <ryuash123@gmail.com>
2021-02-17 18:01:00 +0800

    wip rethinking logic

--- src/components/activities_filter/utils.tsx
+++ src/components/activities_filter/utils.tsx
@@ -3,30 +3,37 @@ export const getFilterLabels = () => {
     {
       key: 'none',
       display: 'none',
+      types: '{}',
     },
     {
       key: 'bank',
       display: 'bank',
+      types: '{cosmos.bank.v1beta1.MsgSend, cosmos.bank.v1beta1.MsgMultiSend}',
     },
     {
       key: 'crisis',
       display: 'crisis',
+      types: '{}',
     },
     {
       key: 'distribution',
       display: 'distribution',
+      types: '{}',
     },
     {
       key: 'governance',
       display: 'governance',
+      types: '{}',
     },
     {
       key: 'slashing',
       display: 'slashing',
+      types: '{}',
     },
     {
       key: 'staking',
       display: 'staking',
+      types: '{}',
     },
   ]);
 };
